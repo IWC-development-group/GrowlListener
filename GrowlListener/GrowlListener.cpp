06e1914f34aab3f01dedc78c0dcde1929f1e09fb
GamePad <hejdjdjuduf@gmail.com>
2026-04-20 14:38:21 +0300

    Base init

---
+++ GrowlListener/GrowlListener.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <miniaudio.h>
+#include <WinSock2.h>
+
+#include "GrowlListener.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
